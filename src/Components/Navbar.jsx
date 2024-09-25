5840f66b20f6d41261aaa5305204df024afd88ae
Stephen Mwangi <mwangiwahome70@gmail.com>
2024-09-25 19:13:14 +0300

    Modified navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 const Navbar = () => {
   return (
     <div className='app__navbar'>
-        <div className="app__navbar-logo"><h1 className='logo'>Duka<span>Smart</span></h1></div>
+        <div className="app__navbar-logo"><h1 className='logo'>Duka<span>_Smart</span></h1></div>
         <div className="app__navbar-links">
            <Link to="/" className='links__main'>Shop</Link>
            <Link to="/cart" className='links__main'><MdOutlineShoppingCart/></Link>
